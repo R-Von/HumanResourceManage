7ffb0f852d42ec39abb731018b80c55d819a4858
R-Von <778615648@qq.com>
2019-10-10 18:17:58 +0800

    chore 注掉个人中心

--- src/components/Tools/UserMenu.vue
+++ src/components/Tools/UserMenu.vue
@@ -1,11 +1,6 @@
 <template>
   <div class='user-wrapper'>
      <div class="content-box">
-      <a href="https://pro.loacg.com/docs/getting-started" target="_blank">
-        <span class="action">
-          <a-icon type="question-circle-o"></a-icon>
-        </span>
-      </a>
       <notice-icon class="action"/>
       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
@@ -14,20 +9,10 @@
         </span>
         <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
           <a-menu-item key="0">
-            <router-link :to="{ name: 'center' }">
+            <!-- <router-link :to="{ name: 'center' }"> -->
               <a-icon type="user"/>
               <span>个人中心</span>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item key="1">
-            <router-link :to="{ name: 'settings' }">
-              <a-icon type="setting"/>
-              <span>账户设置</span>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item key="2" disabled>
-            <a-icon type="setting"/>
-            <span>测试</span>
+            <!-- </router-link> -->
           </a-menu-item>
           <a-menu-divider/>
           <a-menu-item key="3">
