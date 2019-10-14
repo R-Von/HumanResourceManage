eda12a5c514d6905c31855cf8cd443b310d26c54
jocelyn <961951661@qq.com>
2019-10-14 18:05:13 +0800

    feat:add staff-quit

--- src/views/Staff/Quit.vue
+++ src/views/Staff/Quit.vue
@@ -1,15 +1,66 @@
 <template>
-  <a-card>
-    员工管理/离职申请
-
-
-  </a-card>
+  <page-views>
+    <div class="content">
+      <a-card>
+        <a-form
+          :form="form"
+          @submit="handleSubmit"
+        >
+          <a-form-item
+            label="离职时间："
+            :label-col="{ span: 5 }"
+            :wrapper-col="{ span: 12 }"
+            :bind="formItemLayout"
+          >
+            <a-date-picker v-decorator="['date-picker', config]" />
+          </a-form-item>
+          <a-form-item
+            label="Gender"
+            :label-col="{ span: 5 }"
+            :wrapper-col="{ span: 12 }"
+          >
+            <a-select
+              v-decorator="[
+                'gender',
+                {rules: [{ required: true, message: 'Please select your gender!' }]}
+              ]"
+              placeholder="Select a option and change input text above"
+              @change="handleSelectChange"
+            >
+              <a-select-option value="male">
+                male
+              </a-select-option>
+              <a-select-option value="female">
+                female
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item
+            :wrapper-col="{ span: 12, offset: 5 }"
+          >
+            <a-button
+              type="primary"
+              html-type="submit"
+            >
+              Submit
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </a-card>
+    </div>
+  </page-views>
+  
 </template>
 
 <script lang="ts">
 import { Vue , Component } from 'vue-property-decorator'
+import { PageViews } from '@/layouts'
 
-@Component 
+@Component({
+  components:{
+    PageViews
+  }
+})  
 export default class Quit extends Vue{
   private mounted(){
     console.log('Staff  Index')
