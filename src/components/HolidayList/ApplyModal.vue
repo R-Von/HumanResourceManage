518fe54a0ec79548596ac2f707dcb4aea4410276
R-Von <778615648@qq.com>
2019-10-21 08:46:14 +0800

    feat: add something

---
+++ src/components/HolidayList/ApplyModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-modal title="Modal" v-model="visible" @ok="hideModal" okText="确认" cancelText="取消">
+    <p>Bla bla ...</p>
+    <p>Bla bla ...</p>
+    <p>Bla bla ...</p>
+  </a-modal>
+</template>
+
+<script lang="ts">
+import { Vue , Component } from 'vue-property-decorator'
+
+
+export default class ApplyModal extends Vue{
+
+  
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HolidayList/index.ts
@@ -0,0 +1,7 @@
+export function func1(){
+  return 1
+}
+
+export const func2 = ()=>{
+  return 2
+}
